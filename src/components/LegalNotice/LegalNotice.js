27a9055f8854da70f8e7187bb8bb62a664b43722
Prescilla Lecurieux <prescilla.lecurieux@energisme.com>
2021-12-26 14:30:48 +0100

    style(rgpd): fix padding

--- src/components/LegalNotice/LegalNotice.js
+++ src/components/LegalNotice/LegalNotice.js
@@ -5,9 +5,9 @@ import Typography from "@material-ui/core/Typography";
 export default function LegalNotice() {
 
     return (
-        <div className='relative'>
+        <div className='relative px-10'>
             <h1 className='text-center text-xl pt-20'>Mentions Légales et politique de confidentialité</h1>
-            <div className='px-10 pb-5 mt-10'>
+            <div className='pb-5 mt-10'>
                 {rgpd.map(article => (
                     <Typography className="text-white" style={{fontFamily: 'ui-sans-serif, system-ui, "-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto", "Helvetica Neue", "Arial", "Noto Sans", "sans-serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"'}}>
                         <div className='text-lg'>{article.title}</div>
