71746a90466901c17d5e03b39d8802da73b0c8fc
Prescilla Lecurieux <prescilla.lecurieux@energisme.com>
2022-04-21 15:24:17 +0200

    fix stars size

--- src/components/StarsBackground/stars/Stars.tsx
+++ src/components/StarsBackground/stars/Stars.tsx
@@ -5,17 +5,17 @@ import * as random from 'maath/random/dist/maath-random.esm'
 
 const Stars = () => {
     const ref = useRef<any>(null)
-    const [sphere] = useState(() => random.inSphere(new Float32Array(25000), { radius: 1.5 }))
+    const [sphere] = useState(() => random.inSphere(new Float32Array(60000), { radius: 1.5 }))
     useFrame((state, delta) => {
-        ref.current.rotation.x -= delta / 50
-        ref.current.rotation.y -= delta / 50
+        ref.current.rotation.x -= delta / 40
+        ref.current.rotation.y -= delta / 150
     })
     return (
         <group rotation={[0, 0, Math.PI / 30]}>
             {/* @ts-ignore */}
             <Points ref={ref} positions={sphere} stride={3} frustumCulled={false} >
                 {/* @ts-ignore */}
-                <PointMaterial transparent color="#d3531a" size={0.0015} sizeAttenuation depthWrite={false} />
+                <PointMaterial transparent color="#d3531a" size={0.0012} sizeAttenuation depthWrite={false} />
             </Points>
         </group>
     )
